b1576c5ca6259c455831ba46b0d140f056538359
vusakrk <vusalrkh@code.edu.az>
2021-09-01 05:25:24 +0400

    Finish

--- FiorelloAsp/Startup.cs
+++ FiorelloAsp/Startup.cs
@@ -1,6 +1,9 @@
+using FiorelloAsp.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,13 +15,22 @@ namespace FiorelloAsp
 {
     public class Startup
     {
+        public IConfiguration _config { get; }
+
+        public Startup (IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(_config["ConnectionStrings:Default"]);
+        });
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -28,7 +40,7 @@ namespace FiorelloAsp
             }
 
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
